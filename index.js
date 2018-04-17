b63b76db1c017bc148d608dbb33dcb504b9a2281
Carlos Parra <carlos.parra@nearshoresystems.com>
2018-04-16 22:19:04 -0300

    added test for quorum

--- index.js
+++ index.js
@@ -8,16 +8,16 @@ const TokenUtils = require('./tokenUtils');
 const tokenUtils = new TokenUtils(web3, contracts);
 
 /*tokenUtils.transfer(
-  'privateKey',
-  'toAddress',
+  'e6181caaffff94a09d7e332fc8da9884d99902c7874eb74354bdcadf411929f1',
+  '0xbEF31a35048dec439dF293bB5Bd9f6c35BcF32a3',
   1
 ).then(console.log);*/
 
 /*tokenUtils.sendTransaction(
   'erc20',
-  'privateKey',
+  'e6181caaffff94a09d7e332fc8da9884d99902c7874eb74354bdcadf411929f1',
   'transfer',
-  'toAddress', // address
+  '0xbEF31a35048dec439dF293bB5Bd9f6c35BcF32a3', // address
   web3.utils.toWei(String(1), 'ether'), // amount
 ).then(console.log);*/
 
